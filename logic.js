e7a4cd37bd1a54fe60aecbd24e2844298ccd3345
Samuel Mutwiri <smutwiri37@gmail.com>
2024-01-30 19:55:42 +0000

    Structured the code to different scripts for code  reusability

---
+++ logic.js
@@ -0,0 +1,36 @@
+function calculate() {
+    // Get input values
+    var num1 = parseFloat(document.getElementById('num1').value);
+    var num2 = parseFloat(document.getElementById('num2').value);
+
+    // Check if input values are valid numbers
+    if (isNaN(num1) || isNaN(num2)) {
+      alert("Please enter valid numbers");
+      return;
+    }
+
+    // Calculate sum
+    var sum = num1 + num2;
+
+    // Display result
+    document.getElementById('result').innerHTML = "Sum: " + sum;
+  }
+
+function subtract() {
+    // Get input values
+    var num1 = parseFloat(document.getElementById('num1').value);
+    var num2 = parseFloat(document.getElementById('num2').value);
+
+    // Check if input values are valid numbers
+    if (isNaN(num1) || isNaN(num2)) {
+      alert("Please enter valid numbers");
+      return;
+    }
+
+    // subtract
+    var result = num1 - num2;
+
+    // Display result
+    document.getElementById('result').innerHTML = "Result: " + result;
+  
+  }

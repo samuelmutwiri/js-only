b9e06d0519cfdae4f6be79e59bed95b6eaaba301
Samuel Mutwiri <smutwiri37@gmail.com>
2024-02-10 12:28:33 +0000

    Add divide, change UI

--- logic.js
+++ logic.js
@@ -60,4 +60,39 @@ function subtract() {
     // Display result
     document.getElementById('result').innerHTML = "Product: " + product;
   }
-  
+  function divide() {
+    // Get input values
+    var num1 = parseFloat(document.getElementById('num1').value);
+    var num2 = parseFloat(document.getElementById('num2').value);
+
+    // Check if input values are valid numbers and if num2 is not zero
+    if (isNaN(num1) || isNaN(num2) || num2 === 0) {
+        alert("Please enter valid numbers and make sure the second number is not zero");
+        return;
+    }
+
+    // Calculate division
+    var divisionResult = num1 / num2;
+
+    // Display result
+    document.getElementById('result').innerHTML = "Division Result: " + divisionResult;
+}
+function copyToClipboard(value) {
+  // Select the element whose content you want to copy
+  tempInput.value = value;
+
+  // Append the input element to the document body
+  document.body.appendChild(tempInput);
+
+  // Select the input element's content
+  tempInput.select();
+
+  // Copy the selected content to the clipboard
+  document.execCommand("copy");
+
+  // Remove the temporary input element
+  document.body.removeChild(tempInput);
+  // Alert the user that the content has been copied
+  alert("Copied to clipboard!");
+}
+ 
--- pretify.js
+++ pretify.js
@@ -2,3 +2,10 @@ function rotateAndRestore() {
     var element = document.querySelector('.rotating-element');
       element.classList.toggle('rotated');
   }
+  function showcopy() {
+    // Get references to the element
+    var elementToShow = document.getElementById('copyanswer');
+    // Show the button
+    elementToShow.style.display = 'block';
+    
+  }
